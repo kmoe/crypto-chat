764dfc57b095fc686bcad5e5bb191289647876fc
kmoe <k.moe@oxon.org>
2015-07-14 19:16:47 +0100

    make enter button work - close #7

--- src/components/send-message-button.js
+++ src/components/send-message-button.js
@@ -1,16 +1,27 @@
 import React, {
   Component
 } from 'react';
+import _ from 'lodash';
 
 export default class SendMessageButton extends Component {
+  constructor(props) {
+    super(props);
+    this._onClick = _.bind(this.onClick, this);
+  }
+
   render() {
     return (
       <button
         className="pure-button pure-button-primary"
-        type="button"
-        onClick={this.props.onClick}>
+        type="submit"
+        onClick={this._onClick}>
         Encrypt &amp; send
       </button>
     );
+  }
+
+  onClick(e) {
+    e.preventDefault();
+    this.props.onClick();
   }
 }
