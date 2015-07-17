446998de8c61c4c0cde6ff8d93d9d8ddd80ecb6f
kmoe <k.moe@oxon.org>
2015-07-17 21:40:14 +0100

    add explanatory comment

--- src/components/message-panel.js
+++ src/components/message-panel.js
@@ -5,7 +5,8 @@ import _ from 'lodash';
 import MessageName from './message-name';
 import MessageText from './message-text';
 
-//TODO add explanatory comment for this crazy thing
+//input [1, 2, 3]
+//output [1, 1, 2, 2, 3, 3]
 function _piecewiseDuplicate(array) {
   return _.flatten(_.map(array, function(value) {
     return _.fill([0, 0], value);
@@ -16,7 +17,9 @@ export default class MessagePanel extends Component {
 
   render() {
 
-    //N.B. we do not optimistically render pending messages
+    //we can't do this the easy way as <dl>s can't have anything but <dt> and
+    //<dd> as children, and JSX does not permit adjacent elements with no
+    //enclosing element
     const piecewiseDuplicatedMessages = _piecewiseDuplicate(this.props.messages);
     return (
       <dl>
