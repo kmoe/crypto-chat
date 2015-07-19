ff4c14e9b208e22699c790e8ebb254ca4ec43999
kmoe <k.moe@oxon.org>
2015-07-19 18:33:00 +0100

    add decoder for real this time

--- src/crypto/text.js
+++ src/crypto/text.js
@@ -1,3 +1,7 @@
 export function convertPlainTextToArrayBufferView(plaintext) {
   return new TextEncoder('utf-8').encode(plaintext);
 }
+
+export function convertArrayBufferViewToPlainText(arrayBufferView) {
+  return new TextDecoder('utf-8').decode(new Uint8Array(arrayBufferView));
+}
