e396009835641fc202436efb41967458e9def26c
kmoe <k.moe@oxon.org>
2015-07-19 18:27:25 +0100

    add decoder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import App from './components/app';
+import {
+  encryptText
+} from './crypto/api';
 require('style!purecss');
 require('style!./css/css.css');
 
 React.render(<App />, document.getElementById('root'));
+
+encryptText('hello');
