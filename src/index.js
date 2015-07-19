14b53e4aa63f5a252e975dc3205f63334b76b597
kmoe <k.moe@oxon.org>
2015-07-19 18:32:30 +0100

    Revert "add decoder"
    This reverts commit e396009835641fc202436efb41967458e9def26c.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import App from './components/app';
-import {
-  encryptText
-} from './crypto/api';
 require('style!purecss');
 require('style!./css/css.css');
 
 React.render(<App />, document.getElementById('root'));
-
-encryptText('hello');
